4350b7eb4076ba4ad30f778131abd58395242ac4
Gizwanda <adityo.gizwanda@shopee.com>
2023-05-22 11:45:30 +0700

    feat: add html section

---
+++ docs/html/pengenalan.md
@@ -0,0 +1,38 @@
+---
+sidebar_position: 1
+---
+
+# Pengenalan
+
+<p style={{textAlign: 'center'}}><img
+src={require('/img/html5.png').default}
+alt="logo"
+title="HTML5 (HyperText Markup Language) Essentials"
+width="200"
+/></p>
+
+## Apa itu HTML?
+
+HTML (HyperText Markup Language) adalah bahasa markah yang digunakan untuk membuat dan memformat halaman web. HTML digunakan untuk mengatur struktur dan tampilan konten pada halaman web, seperti teks, gambar, video, tautan, tabel, dan elemen-elemen lainnya.
+
+Dalam HTML, elemen-elemen dasar disusun dalam bentuk "tag" yang dikelilingi oleh tanda kurung sudut **(< >)**. Setiap tag HTML memberi tahu browser bagaimana cara menampilkan konten yang terkandung di dalamnya. Sebagian besar elemen HTML memiliki tag pembuka dan penutup, dan konten yang ingin ditampilkan diletakkan di antara kedua tag tersebut.
+
+## Struktur HTML
+Dokumen HTML biasanya terbagi menjadi 2 bagian, **BODY** dan **HEAD**.
+- **HEAD**: Bagian ini biasanya berisi informasi-informasi tentang dokumen HTML terserbut. Seperti judul halaman tersebut, versi HTML yang digunakan, meta data, dll
+- **BODY**: Bagian ini berisikan seluruh hal yang ingin kita tampilkan ke halaman web
+**Contoh Struktur HTML**:
+
+```html
+<html>
+	<head>
+		<title>Pengantar HTML</title>
+	</head>
+
+	<body>
+		<h1>Ini Judul pake H1</h1>
+		Ini deskripsi
+	</body>
+</html>
+```
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Kreatale',
+  tagline: 'Mulai belajar programming di sini',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -39,17 +39,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -64,23 +53,17 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: "Kreatale",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Kreatale Logo',
+          src: 'img/kreatale-icon.svg',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            label: 'Belajar Programming',
           },
         ],
       },
@@ -127,7 +110,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Kreatale. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
